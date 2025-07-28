42377bc985d8b6d2de3274892a970f22e24335cc
georgeygigz <georgeymutti@gmail.com>
2025-07-28 12:41:25 +0300

    refactor log

--- src/main/java/com/georgeygigz/store/aspects/LoggingAspect.java
+++ src/main/java/com/georgeygigz/store/aspects/LoggingAspect.java
@@ -1,50 +1,26 @@
 package com.georgeygigz.store.aspects;
 
-import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.MDC;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Slf4j
 public class LoggingAspect {
 
-    private static final String TRACE_ID = "traceId";
-    @AfterThrowing(
-            pointcut = "execution(* com.georgeygigz.store.controllers..*(..)) || " +
-                    "execution(* com.georgeygigz.store.services..*(..))",
-            throwing = "ex"
-    )
-    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        String traceId = MDC.get(TRACE_ID);
-        String className = joinPoint.getSignature().getDeclaringTypeName();
-        String methodName = joinPoint.getSignature().getName();
-
-        log.info("TraceId: {}, Class: {}, Entering method {} with parameters {}",
-                traceId, className, methodName, joinPoint.getArgs());
-
-        Object result;
-        try{
-            result = joinPoint.proceed();
-        }catch (Throwable throwable){
-            log.error("TraceId: {}, Class: {}, Exception in method {}: {}",
-                    traceId, className, methodName, throwable.getMessage());
-            throw throwable;
-        }
-
-        if (result != null) {
-            log.info("TraceId: {}, Class: {}, Exiting method {} with return value {}",
-                    traceId, className, methodName, result);
-        } else {
-            log.info("TraceId: {}, Class: {}, Exiting method {} with no return value",
-                    traceId, className, methodName);
-        }
-
-        return result;
-
+    @Before("execution(* com.georgeygigz.store.services..*(..))")
+    public void logBeforeMethod(JoinPoint joinPoint) {
+        Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
+        logger.info("Entering method: {}", joinPoint.getSignature().getName());
     }
 
+    @AfterThrowing(pointcut = "com.georgeygigz.store.services..*(..))", throwing = "ex")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable ex) {
+        Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
+        logger.error("Exception in method: {}", joinPoint.getSignature().getName(), ex);
+    }
 }
